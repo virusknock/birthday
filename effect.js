c9b9387b1644af6897bdccada628105c3f3a79c4
Pavan <73213837+virusknock@users.noreply.github.com>
2024-01-04 22:03:10 +0530

    Update effect.js
    Add time

--- effect.js
+++ effect.js
@@ -173,22 +173,22 @@ $('document').ready(function(){
 		var i;
 
 		function msgLoop (i) {
-			$("p:nth-child("+i+")").fadeOut('slow').delay(800).promise().done(function(){
-			i=i+1;
-			$("p:nth-child("+i+")").fadeIn('slow').delay(1000);
-			if(i==50){
-				$("p:nth-child(49)").fadeOut('slow').promise().done(function () {
-					$('.cake').fadeIn('fast');
-				});
-				
-			}
-			else{
-				msgLoop(i);
-			}			
+					$("p:nth-child("+i+")").fadeOut('slow').delay(4000).promise().done(function(){
+					i=i+1;
+					$("p:nth-child("+i+")").fadeIn('slow').delay(5000);
+					if(i==50){
+						$("p:nth-child(49)").fadeOut('slow').promise().done(function () {
+							$('.cake').fadeIn('fast');
+						});
 
-		});
-			// body...
-		}
+					}
+					else{
+						msgLoop(i);
+					}
+
+				});
+					// body...
+				}
 		
 		msgLoop(0);
 		
